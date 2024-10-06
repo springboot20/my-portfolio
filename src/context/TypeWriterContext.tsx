9458f924a5e7c574c1a4e78f24a313ed81eb2725
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-10-06 16:35:48 +0100

    typewritter component modified

--- src/context/TypeWriterContext.tsx
+++ src/context/TypeWriterContext.tsx
@@ -1,64 +1,52 @@
-import React, {
-  useState,
-  useEffect,
-  useMemo,
-  createContext,
-  useRef,
-  useCallback,
-} from 'react'
+import React, { useState, useEffect, useMemo, createContext, useRef, useCallback } from "react";
 
 interface TypeWriterContextProps {
-  textToDisplay: string
+  textToDisplay: string;
 }
 
 export const TypeWriterContext = createContext<TypeWriterContextProps>(
   {} as TypeWriterContextProps,
-)
+);
 
-export const TypeWriterProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const [isDeleting, setIsDeleting] = useState<boolean>(false)
-  const [textToDisplay, setTextToDisplay] = useState<string>('')
-  const wait = useRef<number>(600)
-  const currentWordIndex = useRef<number>(0)
-  const words = useMemo(() => ['Developer', 'Designer'], [])
-  const speed = useRef<number>(900)
+export const TypeWriterProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const [textToDisplay, setTextToDisplay] = useState<string>("");
+  const words = useMemo(() => ["Developer", "Designer", "Expert"], []);
+  const currentWordIndex = useRef<number>(Math.floor(Math.random() * words.length));
 
+  const wait = useRef<number>(600);
+  const speed = useRef<number>(900);
+  
   const type = useCallback(() => {
-    const current = currentWordIndex.current % words.length
-    const fullText = words[current]
+    const current = currentWordIndex.current % words.length;
+    const fullText = words[current];
 
     if (isDeleting) {
-      setTextToDisplay((prev) => fullText.substring(0, prev.length - 1))
+      setTextToDisplay((prev) => fullText.substring(0, prev.length - 1));
     } else {
-      setTextToDisplay((prev) => fullText.substring(0, prev.length + 1))
+      setTextToDisplay((prev) => fullText.substring(0, prev.length + 1));
     }
 
     if (!isDeleting && textToDisplay === fullText) {
-      setIsDeleting(true)
-      speed.current = wait.current
-    } else if (isDeleting && textToDisplay === '') {
-      setIsDeleting(false)
-      currentWordIndex.current++
-      speed.current = 900
+      setIsDeleting(true);
+      speed.current = wait.current;
+    } else if (isDeleting && textToDisplay === "") {
+      setIsDeleting(false);
+      currentWordIndex.current++;
+      speed.current = 900;
     }
-  }, [isDeleting, textToDisplay, words])
+  }, [isDeleting, textToDisplay, words]);
 
   useEffect(() => {
-    setTimeout(type, speed.current)
-  }, [type])
+    setTimeout(type, speed.current);
+  }, [type]);
 
   const values = useMemo(
     () => ({
       textToDisplay,
     }),
     [textToDisplay],
-  )
+  );
 
-  return (
-    <TypeWriterContext.Provider value={values}>
-      {children}
-    </TypeWriterContext.Provider>
-  )
-}
+  return <TypeWriterContext.Provider value={values}>{children}</TypeWriterContext.Provider>;
+};
