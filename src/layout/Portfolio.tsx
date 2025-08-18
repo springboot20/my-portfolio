9bd4cbe10022c13fb0776b15d58e3eb97b19f1f6
springboot20 <abbasopeyemi148@gmail.com>
2025-08-18 21:10:01 +0100

    refactor: clean up unused font-face declarations and optimize CSS structure

--- src/layout/Portfolio.tsx
+++ src/layout/Portfolio.tsx
@@ -76,7 +76,7 @@ export default function PortfolioLayout() {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-10" viewBox="0 0 496 512">
             <path
               d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-              className="fill-port-light-muted dark:fill-[#ABB2BF]"
+              className="fill-port-light-muted dark:fill-white"
             />
           </svg>
         </>
@@ -90,7 +90,7 @@ export default function PortfolioLayout() {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-10" viewBox="0 0 448 512">
             <path
               d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
-              className="fill-port-light-muted dark:fill-[#ABB2BF]"
+              className="fill-port-light-muted dark:fill-white"
             />
           </svg>
         </>
@@ -234,63 +234,87 @@ export default function PortfolioLayout() {
             </motion.header>
 
             <Disclosure.Panel className="lg:hidden fixed transition-all left-0 right-0 w-full z-20 h-full bg-port-light-surface dark:bg-port-bg">
-              <>
-                <div className="p-4 flex items-center justify-between">
-                  <div className="flex items-center space-x-3">
-                    <FontAwesomeIcon
-                      icon={faCode}
-                      className="h-12 text-port-light-primary dark:text-port-primary"
-                    />
-                    <h3 className="text-2xl font-fira-code font-semibold text-port-light-primary dark:text-port-primary">
-                      Abbas Opeyemi
-                    </h3>
+              <div className="flex flex-col justify-between h-3/4">
+                <div className="space-y-10">
+                  <div className="p-4 flex items-center justify-between">
+                    <div className="flex items-center space-x-3">
+                      <FontAwesomeIcon
+                        icon={faCode}
+                        className="h-12 text-port-light-primary dark:text-port-primary"
+                      />
+                      <h3 className="text-2xl font-fira-code font-semibold text-port-light-primary dark:text-port-primary">
+                        Abbas Opeyemi
+                      </h3>
+                    </div>
+
+                    <Disclosure.Button
+                      className={
+                        "text-port-light-text dark:text-white h-10 w-10 flex items-center justify-center"
+                      }
+                    >
+                      <FontAwesomeIcon icon={faClose} className="h-10" strokeWidth={1.5} />
+                    </Disclosure.Button>
                   </div>
+                  <div className="flex flex-col space-y-14 px-4">
+                    {navigations.map(({ path, title }, index) => {
+                      const localIndex = index + navigations.length;
 
-                  <Disclosure.Button
-                    className={
-                      "text-port-light-text dark:text-white h-10 w-10 flex items-center justify-center"
-                    }
-                  >
-                    <FontAwesomeIcon icon={faClose} className="h-10" strokeWidth={1.5} />
-                  </Disclosure.Button>
-                </div>
-                <div className="flex flex-col space-y-14 px-4 mt-10">
-                  {navigations.map(({ path, title }, index) => {
-                    const localIndex = index + navigations.length;
-
-                    return (
-                      <NavLink
-                        to={path}
-                        key={`${title}-${localIndex}`}
-                        className="group"
-                        onClick={() => close()}
-                      >
-                        {({ isActive }) => (
-                          <motion.div
-                            variants={{ ...listItem(localIndex) }}
-                            initial="hidden"
-                            animate="visible"
-                          >
-                            <p
-                              className={classNames(
-                                "font-medium font-fira-code text-4xl group-hover:text-port-light-text dark:group-hover:text-white",
-                                isActive
-                                  ? "text-port-light-text dark:text-white font-medium"
-                                  : "text-port-light-muted dark:text-port-gray font-normal"
-                              )}
+                      return (
+                        <NavLink
+                          to={path}
+                          key={`${title}-${localIndex}`}
+                          className="group"
+                          onClick={() => close()}
+                        >
+                          {({ isActive }) => (
+                            <motion.div
+                              variants={{ ...listItem(localIndex) }}
+                              initial="hidden"
+                              animate="visible"
                             >
-                              <span className="text-port-light-primary dark:text-port-primary">
-                                #
-                              </span>
-                              {splitText(title)}
-                            </p>
-                          </motion.div>
-                        )}
-                      </NavLink>
-                    );
-                  })}
+                              <p
+                                className={classNames(
+                                  "font-medium font-fira-code text-4xl group-hover:text-port-light-text dark:group-hover:text-white",
+                                  isActive
+                                    ? "text-port-light-text dark:text-white font-medium"
+                                    : "text-port-light-muted dark:text-port-gray font-normal"
+                                )}
+                              >
+                                <span className="text-port-light-primary dark:text-port-primary">
+                                  #
+                                </span>
+                                {splitText(title)}
+                              </p>
+                            </motion.div>
+                          )}
+                        </NavLink>
+                      );
+                    })}
+                  </div>
                 </div>
-              </>
+
+                <div className="flex items-center justify-center space-x-4 py-10">
+                  <Link to="https://github.com/springboot20" className="inline-block shrink-0">
+                    <span className="sr-only">Github</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10" viewBox="0 0 496 512">
+                      <path
+                        d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+                        className="fill-port-light-muted dark:fill-white"
+                      />
+                    </svg>
+                  </Link>
+                  <Link to="" className="inline-block shrink-0">
+                    <span className="sr-only">Linkedin</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10" viewBox="0 0 448 512">
+                      <path
+                        d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+                        className="fill-port-light-muted dark:fill-white"
+                      />
+                    </svg>
+                  </Link>
+                  <Link to=""></Link>
+                </div>
+              </div>
             </Disclosure.Panel>
 
             <motion.main>
@@ -337,7 +361,7 @@ export default function PortfolioLayout() {
                               >
                                 <path
                                   d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-                                  className="fill-port-light-muted dark:fill-[#ABB2BF]"
+                                  className="fill-port-light-muted dark:fill-white"
                                 />
                               </svg>
                             </Link>
@@ -350,7 +374,7 @@ export default function PortfolioLayout() {
                               >
                                 <path
                                   d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
-                                  className="fill-port-light-muted dark:fill-[#ABB2BF]"
+                                  className="fill-port-light-muted dark:fill-white"
                                 />
                               </svg>
                             </Link>
--- src/pages/about/about-me.tsx
+++ src/pages/about/about-me.tsx
@@ -32,17 +32,71 @@ const skills = {
   },
 };
 
+const titleVariants = {
+  initial: {
+    opacity: 0,
+    y: 15,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const lineVariants = {
+  initial: {
+    opacity: 0,
+    width: 0,
+  },
+  animate: {
+    opacity: 1,
+    width: "50%",
+  },
+};
+
 export default function AboutPageComponent() {
   return (
     <article className="w-full py-28">
       <div className="w-full">
         <header className="space-y-3">
-          <h1 className="font-fira-code font-semibold text-3xl text-port-light-text dark:text-white">
+          <motion.h1
+            initial={{
+              opacity: 0,
+              y: 10,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: "spring",
+                delay: 0.3,
+              },
+            }}
+            className="font-fira-code font-semibold text-3xl text-port-light-text dark:text-white"
+          >
             <span className="text-port-light-primary dark:text-port-primary">/</span>about me
-          </h1>
-          <p className="text-port-light-text dark:text-white font-fira-code font-normal text-lg">
+          </motion.h1>
+          <motion.p
+            variants={{
+              initial: {
+                opacity: 0,
+                y: 20,
+              },
+              animate: {
+                opacity: 1,
+                y: 0,
+                transition: {
+                  type: "spring",
+                  delay: 0.5,
+                },
+              },
+            }}
+            initial="initial"
+            animate="animate"
+            className="text-port-light-text dark:text-white font-fira-code font-normal text-lg"
+          >
             who am i?
-          </p>
+          </motion.p>
         </header>
 
         <section className="py-10">
@@ -91,10 +145,38 @@ export default function AboutPageComponent() {
         <section className="py-10">
           <div className="w-full space-y-10">
             <div className="flex flex-col space-y-2 sm:space-y-0 sm:flex-row sm:items-center sm:space-x-2 shrink-0 flex-1">
-              <h2 className="text-port-light-text dark:text-white font-medium font-fira-code text-2xl">
+              <motion.h2
+                variants={{
+                  ...titleVariants,
+                  animate: {
+                    ...titleVariants.animate,
+                    transition: {
+                      delay: 0.6,
+                    },
+                  },
+                }}
+                initial="initial"
+                whileInView="animate"
+                viewport={{ amount: 0.7, once: false }}
+                className="text-port-light-text dark:text-white font-medium font-fira-code text-2xl"
+              >
                 <span className="text-port-light-primary dark:text-port-primary">#</span>skills
-              </h2>
-              <div className="h-0.5 w-full sm:w-1/2 bg-port-light-primary dark:bg-port-primary block" />
+              </motion.h2>
+              <motion.div
+                variants={{
+                  ...lineVariants,
+                  animate: {
+                    ...lineVariants.animate,
+                    transition: {
+                      delay: 0.4,
+                    },
+                  },
+                }}
+                initial="initial"
+                whileInView="animate"
+                viewport={{ amount: "all", once: false }}
+                className="h-0.5 w-full sm:w-1/2 bg-port-light-primary dark:bg-port-primary block"
+              />
             </div>
 
             <div className="grid grid-cols-2 xl:grid-cols-3 flex-1 gap-3">
